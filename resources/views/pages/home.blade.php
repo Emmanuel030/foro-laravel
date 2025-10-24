2d536ec8ad08b00fba35281fc9e19fdfc1118886
Emmanuel Choxom <emmanuelchoxom03@umes.edu.gt>
2025-10-24 01:24:22 -0600

    Actualizar diseño o contenido del home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,16 +1,35 @@
 <x-forum.layouts.home>
-    <h1 class="text-3xl font-bold text-center mt-10">Foro Laravel funcionando 🚀</h1>
+<ul class="divide-y divide-gray-100">
+@if (session('success'))
+    <div class="max-w-2xl mx-auto my-4 p-4 bg-green-600 text-white text-center rounded-lg shadow">
+         {{ session('success') }}
+    </div>
+@endif
 
-    @if(isset($questions) && count($questions))
-        <div class="mt-6 max-w-3xl mx-auto">
-            @foreach($questions as $question)
-                <div class="bg-gray-800 p-4 rounded-lg mb-3">
-                    <h2 class="text-xl font-semibold text-white">{{ $question->title }}</h2>
-                    <p class="text-gray-400">{{ $question->content }}</p>
-                </div>
-            @endforeach
+@foreach($questions as $question)
+    <li class="flex justify-between gap-4 py-4">
+      <div class="flex gap-4">
+        <div class="size-8 rounded-full flex items-center justify-center"  style="background-color: {{ $question->category->color ?? '#999' }};">
+          <x-forum.logo class="h-6 text-white" />
         </div>
-    @else
-        <p class="text-center text-gray-400 mt-6">No hay preguntas todavía 💤</p>
-    @endif
+  
+        <div class="flex-auto">
+          <p class="text-sm font-semibold text-gray-900">
+            <a href="{{ route('questions.show', $question) }}" class="hover:underline">
+              {{ $question->title }}
+            </a>
+          </p>
+          <p class="mt-1 text-xs text-gray-500">{{ $question->user->name }}</p>
+        </div>
+      </div>
+  
+      <div class="hidden sm:flex sm:flex-col sm:items-end">
+        <p class="text-sm text-gray-900">{{ $question->category->name ?? 'Sin categoría' }}</p>
+        <p class="mt-1 text-xs text-gray-500">{{ $question->created_at->diffForHumans() }}</p>
+      </div>
+    </li>
+    @endforeach
+  
+  </ul>
+  <!-- component: forum/layouts/home -->
 </x-forum.layouts.home>
